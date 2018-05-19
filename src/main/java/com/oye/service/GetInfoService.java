c2146cf8d8622462cf487d832ee45be60029d68e
ys <>
2018-05-19 18:19:02 +0900

    clean up methods

--- src/main/java/com/oye/service/GetInfoService.java
+++ src/main/java/com/oye/service/GetInfoService.java
@@ -15,20 +15,20 @@ import org.springframework.stereotype.Service;
 @Service
 public class GetInfoService {
 
-	private String commonGetText(String url) {
-		Document document;
-		try {
-			document = Jsoup.connect(url).get();
-			return document.body().toString();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "nothing";
+	private ArrayList<String> commonGetMatcher(String url, String regex) throws IOException {
+		Document document = Jsoup.connect(url).get();
+		Pattern p = Pattern.compile(regex);
+		Matcher m = p.matcher(document.body().toString());
+		ArrayList<String> list = new ArrayList<>();
+		for (int i = 1; i <= 10; i++) {
+			m.find();
+			list.add(m.group(2));
 		}
-
+		return list;
 	}
 
 	// コラム数により試行回数増減
-	public ArrayList<String> couldNotConnect() {
+	private ArrayList<String> couldNotConnect() {
 		ArrayList<String> failedList = new ArrayList<>();
 		for (int i = 0; i < 10; i++) {
 			failedList.add("情報を取得できませんでした。");
@@ -44,38 +44,19 @@ public class GetInfoService {
 
 	// twitter
 	public ArrayList<String> getTwInfoLogic() {
-		String text = commonGetText("https://twittrend.jp/japan/");
-		if(text.equals("nothing")){
+		try {
+			return commonGetMatcher("https://twittrend.jp/japan/", "(\\d\\.<.+>)(.+)</a");
+		} catch (IOException e) {
+			e.printStackTrace();
 			return couldNotConnect();
 		}
-		ArrayList<String> twList = new ArrayList<>();
-		for (int i = 1; i <= 10; i++) {
-			Pattern p = Pattern.compile("(" + i + "\\.<.+>)(.+)</a");
-			Matcher m = p.matcher(text);
-			m.find();
-			twList.add(m.group(2));
-		}
-		return twList;
-
 	}
 
 	// google
 	public ArrayList<String> getGoogleInfoLogic() {
-		Document document;
 		try {
-			document = Jsoup.connect("https://trends.google.co.jp/trends/hottrends/atom/hourly?pn=p4").get();
-			String googleText = document.body().toString();
-			String text = commonGetText("https://trends.google.co.jp/trends/hottrends/atom/hourly?pn=p4");
-			Pattern p = Pattern.compile(";!\\[CDATA\\[(.+),");
-			Matcher m = p.matcher(text);
-			m.find();
-			googleText = m.group(1);
-			String[] googles = googleText.split(", ", 0);
-			ArrayList<String> googleList = new ArrayList<>();
-			for (int i = 0; i < googles.length; i++) {
-				googleList.add(googles[i]);
-			}
-			return googleList;
+			return commonGetMatcher("https://trends.google.co.jp/trends/hottrends/atom/hourly?pn=p4",
+					"(<a.+>)(.+)</a>");
 		} catch (IOException e) {
 			e.printStackTrace();
 			return couldNotConnect();
@@ -84,9 +65,8 @@ public class GetInfoService {
 
 	// nhk
 	public ArrayList<String> getNHKInfoLogic() {
-		Element element;
 		try {
-			element = Jsoup.connect("http://www3.nhk.or.jp/rss/news/cat0.xml").get();
+			Element element = Jsoup.connect("http://www3.nhk.or.jp/rss/news/cat0.xml").get();
 			Elements elements = element.getElementsByTag("title");
 			ArrayList<String> nhkList = new ArrayList<>();
 			for (int i = 1; i <= 7; i++) {
@@ -101,16 +81,7 @@ public class GetInfoService {
 
 	public ArrayList<String> getITmediaInfoLogic(String regex) {
 		try {
-			Document doc = Jsoup.connect("http://www.itmedia.co.jp/news/subtop/archive/").get();
-			String text = doc.body().toString();
-			Pattern p = Pattern.compile(regex);
-			Matcher m = p.matcher(text);
-			ArrayList<String> list = new ArrayList<>();
-			for (int i = 1; i <= 10; i++) {
-				m.find();
-				list.add(m.group(2));
-			}
-			return list;
+			return commonGetMatcher("http://www.itmedia.co.jp/news/subtop/archive/", regex);
 		} catch (IOException e) {
 			e.printStackTrace();
 			return couldNotConnect();
